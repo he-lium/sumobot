cd6801ff6c05b4cc12caa4cee6599fe048f169a5
Dominic He <dominiche2010@gmail.com>
2017-09-25 15:22:34 +1000

    Switch to using interrupt for ultrasonic

--- sumobot.ino
+++ sumobot.ino
@@ -10,7 +10,7 @@ const int toggleButton = 10;
 // Motor A
 const int motorApin1 = 9;
 const int motorApin2 = 8; // Reverse direction
-const int motorAspeed = 3; // PWM
+const int motorAspeed = 4; // PWM
 
 // Motor B
 const int motorBpin1 = 6;
@@ -20,8 +20,15 @@ const int motorBspeed = 5; // PWM
 // Ultrasonic 1
 const int trigPin = 2;
 const int echoPin = 0;
-unsigned long ultrasonicTimeSinceLastRead = 0;
-int ultrasonicThreshold = 20;
+unsigned int us1Duration = 0; // time taken (millis) for ultrasonic to read
+unsigned int us1Read = 0; // counter for num of ultrasonic measures
+
+// Ultrasonic 2
+const int trigPin2 = 15;
+const int echoPin2 = 3;
+
+// Ultrasonic Distance in cm for changing between search and attack
+int ultrasonicThreshold = 40;
 
 const int waitingTime = 3000;
 
@@ -49,6 +56,8 @@ void setup() {
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
     digitalWrite(trigPin, LOW);
+    // Ultrasonic 1 Interrupt
+    attachInterrupt(digitalPinToInterrupt(echoPin), ultrasonic1_echo, CHANGE);
 
     // Set up state
     state = off;
@@ -94,21 +103,33 @@ void loop() {
 
 // decide what to do when playing
 void decidePlay() {
+    // ultrasonic update counter
+    static unsigned long currentUs1Read = 0;
+    // time (millis) since last ultrasonic read
+    static unsigned long ultrasonicTimeSinceLastRead = 0;
+
+    // ultrasonic trigger
+    if (millis() - ultrasonicTimeSinceLastRead > 7) {
+        // start new ultrasonic read
+        trigUltrasonic(trigPin);
+        ultrasonicTimeSinceLastRead = millis();
+    }
+
     switch(playState) {
     case search:
         runMotors(1, 0, 140, 140);
-        // Take new reading from ultrasonic
-        if (millis() - ultrasonicTimeSinceLastRead > 5) {
-            unsigned long startTime = millis();
-            int distance = measureUltrasonic();
-            // Serial.println(millis() - startTime);
-            // Serial.print("Distance: ");
-            // Serial.println(distance);
-            ultrasonicTimeSinceLastRead = millis();
+        if (currentUs1Read != us1Read) {
+            // new update from us1
+            currentUs1Read = us1Read;
+            // calculate distance from ultrasonic
+            int distance = us1Duration / 58;
+            Serial.print("search: ");
+            Serial.println(distance);
 
             if (distance < ultrasonicThreshold) {
                 filter++;
-                if (filter >= 10) {
+                if (filter >= 10) { // if distance is consistently above threshold
+                    // Change state: attack
                     filter = 0;
                     playState = attack;
                     Serial.println("Distance below threshold; attack");
@@ -120,17 +141,19 @@ void decidePlay() {
         break;
     case attack:
         runMotors(1, 1, 140, 140);
-        if (millis() - ultrasonicTimeSinceLastRead > 5) {
-            unsigned long startTime = millis();
-            int distance = measureUltrasonic();
-            // Serial.println(millis() - startTime);
-            // Serial.print("Distance: ");
-            // Serial.println(distance);
-            ultrasonicTimeSinceLastRead = millis();
-
+        if (currentUs1Read != us1Read) {
+            // new update from us1
+            currentUs1Read = us1Read;
+            // calculate distance from ultrasonic
+            int distance = us1Duration / 58;
+            Serial.print("attack: ");
+            Serial.println(distance);
+            
             if (distance > ultrasonicThreshold) {
+                // Distance starting to be greater than threshold
                 filter++;
-                if (filter >= 10) {
+                if (filter >= 10) { // if distance is consistently above threshold
+                    // Change state: search
                     filter = 0;
                     playState = search;
                     Serial.println("Distance above threshold; search");
@@ -192,4 +215,29 @@ int measureUltrasonic() {
     
     // read sig from sensor, convert into dist
     return pulseIn(echoPin, HIGH) / 58;
+}
+
+// Sends trigger to activate ultrasonic
+int trigUltrasonic(int trigger) {
+    // sensor is triggered by HIGH pulse of 10 or more seconds
+    // give short LOW pulse beforehand
+    digitalWrite(trigger, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+}
+
+// interrupt call when ultrasonic1 starts echo
+void ultrasonic1_echo() {
+    static volatile unsigned int startTime;
+    switch (digitalRead(echoPin)) {
+    case HIGH: // start of echo pulse
+        startTime = micros();
+        break;
+    case LOW: // end of echo pulse; record duration
+        us1Duration = micros() - startTime;
+        us1Read++;
+        break;
+    }
 }
